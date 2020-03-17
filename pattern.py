276f2823d86560582bd723f6729860da9c0e220c
Amit Kumar Chaudhary <53606021+amitchaudharycs18@users.noreply.github.com>
2020-03-17 11:24:02 +0530

    hii

---
+++ pattern.py
@@ -0,0 +1,11 @@
+a=int(input());
+for i in range(0,a):
+    print("1",end='');
+print();
+for i in range(0,a-2):
+    print("1",end='');
+    for j in range(0,a-2):
+        print(" ",end='');
+    print("1");
+for i in range(0,a):
+    print("1",end='');
